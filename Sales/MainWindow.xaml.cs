45841f27620d03bd69683873cc4ef8e81dbbe492
Denys Samoilenko <denniksam@gmail.com>
2023-01-17 19:42:03 +0200

    Add project files.

---
+++ Sales/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data.SqlClient;   // Подключаем ADO для MS SQL Server (не забыть NuGet)
+
+namespace Sales
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SqlConnection _connection;  // объект-подключение к БД
+        public MainWindow()
+        {
+            InitializeComponent();
+            // строка подключения - берется из свойств БД (Server Explorer)
+            String connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\_dns_\source\repos\ADO-111\Sales\Sales111.mdf;Integrated Security=True";
+            // создание объекта-подключения !! не открывает подключение
+            _connection = new(connectionString);
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _connection.Open();  // открытие подключения
+                MonitorConnection.Content = "Установлено";
+                MonitorConnection.Foreground = Brushes.Green;
+            }
+            catch(SqlException ex)
+            {
+                MonitorConnection.Content = "Закрыто";
+                MonitorConnection.Foreground = Brushes.Red;
+                MessageBox.Show(ex.Message);
+                this.Close();
+            }
+            ShowDepartmentsCount();
+        }
+
+        /// <summary>
+        /// Выводит в таблицу-монитор количество отделов (департаментов) из БД
+        /// </summary>
+        private void ShowDepartmentsCount()
+        {
+            String sql = "SELECT COUNT(*) FROM Departments";
+            // SqlCommand объект для передачи команд (запросов) к БД.
+            // Требует закрытия, поэтому using
+            using var cmd = new SqlCommand(sql, _connection);
+            // создание объекта не исполняет команду, для этого есть методы ExecuteXxxx
+            MonitorDepartments.Content =
+                Convert.ToString(
+                    cmd.ExecuteScalar()   // выполняет команду и возвращает "верхний-левый" результат
+                );
+        }
+    }
+}
---
+++ Sales/SQL/Departments.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Departments (
+	Id			UNIQUEIDENTIFIER  NOT NULL  PRIMARY KEY,
+	Name		NVARCHAR(50)      NOT NULL
+) ;
+
+INSERT INTO Departments 
+	( Id, Name )
+VALUES 
+	( 'D3C376E4-BCE3-4D85-ABA4-E3CF49612C94',  N'IT отдел'		 	 ), 
+	( '131EF84B-F06E-494B-848F-BB4BC0604266',  N'Бухгалтерия'		 ), 
+	( '8DCC3969-1D93-47A9-8B79-A30C738DB9B4',  N'Служба безопасности'), 
+	( 'D2469412-0E4B-46F7-80EC-8C522364D099',  N'Отдел кадров'		 ),
+	( '1EF7268C-43A8-488C-B761-90982B31DF4E',  N'Канцелярия'		 ), 
+	( '415B36D9-2D82-4A92-A313-48312F8E18C6',  N'Отдел продаж'		 ), 
+	( '624B3BB5-0F2C-42B6-A416-099AAB799546',  N'Юридическая служба' )
