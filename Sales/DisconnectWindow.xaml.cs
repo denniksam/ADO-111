01d9204b159c7f04988b67f0e738df7393457b13
Denys Samoilenko <denniksam@gmail.com>
2023-01-31 19:38:27 +0200

    Disc--2

--- Sales/DisconnectWindow.xaml.cs
+++ Sales/DisconnectWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,12 +22,80 @@ namespace Sales
     public partial class DisconnectWindow : Window
     {
         public ObservableCollection<Entities.Department> Departments { get; set; }
+        public ObservableCollection<Entities.Product> Products { get; set; }
         public DisconnectWindow()
         {
             InitializeComponent();
             // Связывание. Часть 1. Контекст
             DataContext = this;  // Представление получает доступ к всему объекту окна
+            using SqlConnection connection = new(App.ConnectionString);
+            try
+            {
+                connection.Open();
 
+                #region Departments
+                Departments = new();
+                using SqlCommand cmd = new("SELECT Id, Name FROM Departments", connection);
+                {
+                    using var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Departments.Add(new()  // Изменение коллекции отобразиться на ListView
+                        {
+                            Id = reader.GetGuid(0),
+                            Name = reader.GetString(1)
+                        });
+                    }
+                }
+                #endregion
+
+                #region Products
+                Products = new();
+                using SqlCommand cmd2 = new("SELECT Id, Name, Price FROM Products", connection);
+                {
+                    using var reader = cmd2.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Products.Add(new()  // Изменение коллекции отобразиться на ListView
+                        {
+                            Id = reader.GetGuid(0),
+                            Name = reader.GetString(1),
+                            Price = reader.GetDouble(2)
+                        });
+                    }
+                }
+                #endregion
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(sender is ListViewItem item)  // item = sender as ListViewItem
+            {
+                // Обратная связь (view->model) через item.Content
+                if (item.Content is Entities.Department department)
+                {
+                    MessageBox.Show(department.ToShortString());
+                }
+            }
+            
+        }
+
+        private void ListViewItem_MouseDoubleClick_1(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListViewItem item)  // item = sender as ListViewItem
+            {
+                // Обратная связь (view->model) через item.Content
+                if (item.Content is Entities.Product product)
+                {
+                    MessageBox.Show(product.ToShortString());
+                }
+            }
         }
     }
 }
