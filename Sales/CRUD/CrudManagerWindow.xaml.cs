57544404db75f5872cad179de3eaa3dfde72cc17
Denys Samoilenko <denniksam@gmail.com>
2023-02-07 19:36:41 +0200

    CrudManagerWindow

--- Sales/CRUD/CrudManagerWindow.xaml.cs
+++ Sales/CRUD/CrudManagerWindow.xaml.cs
@@ -29,5 +29,40 @@ namespace Sales.CRUD
         {
             DataContext = (Owner as DisconnectWindow);
         }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (DepartmentsCombo.SelectedValue is Entities.Department department)
+            {
+                Guid id = department.Id;
+                MessageBox.Show(
+                    $"{ManagerSurname.Text} - {ManagerName.Text} - {ManagerSecname.Text} - {id}"
+                );
+            }
+            else
+            {
+                MessageBox.Show("Выберите отдел");
+            }
+            
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
+/* Завершить разработку окна CrudManagerWindow
+ * - организовать свойство Manager
+ * - реализовать его начальную проверку на null (признак создания)
+ *    в таком случае генерировать новый идентификатор
+ * - иначе (не null) перенести данные из полей Manager в интерфейс
+ * - по нажатию кнопки "сохранить" обеспечить проверку необходимых данных
+ *    (на пустоту), в случае ошибки выдать предупреждение
+ * ** рядом с комбобоксами "совместитель" и "шеф" добавить кнопки "сбросить"   
+ */
